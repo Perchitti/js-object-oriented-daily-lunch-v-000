066b28468d2d2167c6efb344a898e79a38ba8a71
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-23 15:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ class Customer {
 
   meals() {
     return this.deliveries().filter(function (delivery) {
-      if(delivery.meal !== delivery.meal){
+      if(delivery.mealId !== delivery.mealId){
         return delivery.meal
       }
     });
